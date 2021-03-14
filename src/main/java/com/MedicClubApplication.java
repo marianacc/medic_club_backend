48a07b438a9c05d8f17cfc6759b7e0a87d62c2e4
mari <mcardozocosta@hotmail.com>
2021-03-14 19:04:00 -0400

    Creacion del proyecto e implementacion de librerias

---
+++ src/main/java/com/MedicClubApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicClubApplication.class, args);
+	}
+
+}
